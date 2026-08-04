c8ca1137e22996fc19b881fd0df97f23e151d1d9
rajafarhan56890-coder <rajafarhan56890@gmail.com>
2026-08-04 11:35:21 +0500

    feat: Add sidebar state management for mobile responsiveness

---
+++ src/store/sidebarStore.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+interface SidebarStore {
+  isOpen: boolean;
+  openSidebar: () => void;
+  closeSidebar: () => void;
+  toggleSidebar: () => void;
+}
+
+export const useSidebarStore = create<SidebarStore>((set) => ({
+  isOpen: false,
+  openSidebar: () => set({ isOpen: true }),
+  closeSidebar: () => set({ isOpen: false }),
+  toggleSidebar: () => set((state) => ({ isOpen: !state.isOpen })),
+}));
